accaa822ed541083ab10ecf377eb0bf98c470c87
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-09 16:05:22 +0200

    path parameters

--- app/app.py
+++ app/app.py
@@ -4,38 +4,19 @@ from enum import Enum
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-class Item(BaseModel):
-    name : str
-    price : float
-    is_offer : bool | None = None
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-## PATH PARAMETERS
-@app.get("/items/current")
-async def read_item_current():
-    return {"item_id": "current item"}
+@app.get("/items/")
+async def read_item_list(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    if model_name.value == ModelName.lenet:
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    return {"model_name": model_name, "message": "Have some residuals"}
+async def read_item(item_id: int, q: str|None = None, short: bool = True):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
