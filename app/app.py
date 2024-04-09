bde92995a3b076f0d488ce5abb0dce11417dbe95
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-09 17:02:18 +0200

    request body

--- app/app.py
+++ app/app.py
@@ -1,22 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from enum import Enum
+
+class Item(BaseModel):
+    name: str
+    description: str|None = None
+    price: float
+    tax: float|None = None
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
-@app.get("/items/")
-async def read_item_list(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str|None = None, short: bool = True):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str|None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q: 
+        result.update({"q": q})
+    return result
