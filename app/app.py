dedf50268be3cc2c8f9a8301ac849933a3e93867
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-09 15:37:11 +0200

    path parameters

--- app/app.py
+++ app/app.py
@@ -1,9 +1,41 @@
-from flask import Flask,jsonify  # Import the Flask class
-app = Flask(__name__)    # Create an instance of the class for our use
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
 
-@app.route("/", methods=["GET"])
-def say_hello():
-    return jsonify({"msg": "Hello from Flask"})
+app = FastAPI()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+class Item(BaseModel):
+    name : str
+    price : float
+    is_offer : bool | None = None
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+## PATH PARAMETERS
+@app.get("/items/current")
+async def read_item_current():
+    return {"item_id": "current item"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == ModelName.lenet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
