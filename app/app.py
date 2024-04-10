2d1213b48c176ab03f427f2cf28bbe41d76bb647
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-10 15:03:47 +0200

    body - fields

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, Query, Path, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Annotated
 
 class Item(BaseModel):
     name: str
-    description: str|None = None
-    price: float
+    description: str|None = Field(default=None, title="Description of the item", max_length=300)
+    price: float = Field(gt=0)
     tax: float|None = None
 
 class User(BaseModel):
@@ -30,3 +30,4 @@ async def update_item(item_id: Annotated[int|None, Path(lt=100)],
     if user:
         results.update({"user": user})
     return results
+
