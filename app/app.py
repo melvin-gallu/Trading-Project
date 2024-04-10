d79d9c629d523d58aba50298fd4b2523472db654
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-10 15:22:13 +0200

    body - nested models

--- app/app.py
+++ app/app.py
@@ -2,32 +2,28 @@ from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel, Field
 from typing import Annotated
 
+
+class Image(BaseModel):
+    url: str
+    name: str|None = None
+
 class Item(BaseModel):
     name: str
     description: str|None = Field(default=None, title="Description of the item", max_length=300)
     price: float = Field(gt=0)
     tax: float|None = None
-
-class User(BaseModel):
-    username: str
-    full_name: str|None = None
+    tags: list[int] = []
+    tags_unique: set[int] = set() #set contains only unique values
+    image: Image|None = None #nest another class in *this
 
 app = FastAPI()
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: Annotated[int|None, Path(lt=100)], 
-                      q: Annotated[str|None, Query()] = None,
-                      item: Item|None = None, user: User|None = None,
-                      importance: Annotated[int, Body(le=5)] = 5):
+                      item: Item|None = None):
     """
-    Path parameter + query parameter + multiple body parameters
+    Body parameter with nested list inside the class
     """
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    if user:
-        results.update({"user": user})
+    results = {"item_id": item_id, "item": item}
     return results
 
