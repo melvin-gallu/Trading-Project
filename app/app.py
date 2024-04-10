b9940f90326d729388cf37ac809ac17dd1bd9b88
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-10 14:11:55 +0200

    path parameters and numeric validations

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from pydantic import BaseModel
 from typing import Annotated
 
@@ -10,40 +10,12 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-@app.get("/items/")
-async def read_items(q: Annotated[str|None, Query(max_length=50)] = None):
+@app.get("/items/{item_id}")
+async def read_items(item_id: Annotated[int, Path(title="The ID of the item", ge=3)], q: Annotated[str|None, Query()] = None):
     """
-    Specifiy condition for validation using annotated query parameter
+    Path parameter with specific conditions for valid argument
     """
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    results = {"items_id": item_id}
     if q:
         results.update({"q":q})
     return results
-
-@app.get("/items/bis/")
-async def read_items(q: Annotated[str|None, Query(min_length=3,max_length=50,pattern="^fixedquery$")] = None):
-    """
-    Specify a regex expression to use in the annotated query parameter
-    """
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q":q})
-    return results
-
-@app.get("/items/tris/")
-async def read_items(q: Annotated[str, Query(min_length=3,max_length=50)]):
-    """
-    Make query parameter required
-    """
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q":q})
-    return results
-@app.get("/items/list/")
-async def read_items_list(q: Annotated[list[str]|None, Query()] = ["Mel","vin"]):
-    """
-    Query parameter defined as a list of string
-    Call it via /items/list/?q=item1&q=item2
-    """
-    query_items = {"q": q}
-    return query_items
