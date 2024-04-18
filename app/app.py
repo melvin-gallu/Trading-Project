8c3f73565338205ecd7b5e7639fa72f1e7a9a47e
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-18 16:30:13 +0200

    form data

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.post("/login/")
 async def login(username: Annotated[str, Form()], pwd: Annotated[str, Form()]):
     """
-    he way HTML forms (<form></form>) sends the data to the server normally uses a "special" encoding for that data, 
+    The way HTML forms sends the data to the server normally uses a "special" encoding for that data, 
     it's different from JSON.
     """
     return {"username": username}
