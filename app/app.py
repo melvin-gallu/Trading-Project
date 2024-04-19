0af76addfdac5202a248c33a61971fe1114e8241
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-19 14:10:18 +0200

    global dependencies

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from typing import Annotated
 
-app = FastAPI()
+
 
 async def verify_token(x_token: str = Header()):
     if x_token != "fake-super-secret-token":
@@ -16,11 +16,4 @@ async def verify_key(x_key: str = Header()):
         raise HTTPException(status_code=400, detail="X-Key header invalid")
     return x_key
     
-
-@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
-async def read_items():
-    """
-    Use dependencies in the path decorator
-    """
-    return {"message": "key and token valid"}
-
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)]) #add global dependecies to the whole app
