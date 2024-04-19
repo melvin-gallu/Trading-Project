c9834d9e0d87e5ab82f6ab964b2bddaae5e55945
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-19 12:50:46 +0200

    body - updates

--- app/app.py
+++ app/app.py
@@ -1,23 +1,38 @@
 from datetime import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Body
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-
-fake_db = {}
+from typing import Annotated
 
 class Item(BaseModel):
-    title: str
-    timestamp: datetime
+    name: str|None = None
     description: str|None = None
+    price: float|None = None
+    tax: float = 10.5
+    tags: list[str] = []
 
 app = FastAPI()
 
-@app.put("/items/{id}")
-async def update_item(id: str, item: Item):
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get("/items/{id}")
+async def read_item(id: Annotated[str|None, Path()]):
+    return items[id]
+
+
+@app.patch("/items/{id}", response_model=Item)
+async def udpdate_item(id: Annotated[str|None, Path()], item: Annotated[Item, Body()]):
     """
-    Convert a Pydantic model to a dict, and the datetime to a str, to suit the database needs
+    Update partially an item using **exclude_unset**
     """
-    json_comaptible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_comaptible_item_data
-    return {"db": fake_db}
+    stored_item_data = items[id] #retrieved stored data
+    stored_item_model = Item(**stored_item_data) #convert data t Pydantic model
+    udpdate_data = item.model_dump(exclude_unset=True) #generate dict without default values from input model
+    updated_item = stored_item_model.model_copy(update=udpdate_data) #create a copy of stored model, update its attributes with received partial updates
+    items[id] = jsonable_encoder(updated_item) #convert the copied model to something that can be stored in a DB
+    return updated_item
