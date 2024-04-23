41b6f60f875541097e8461bfff2f565db4aff9ff
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-23 09:33:19 +0200

    middleware

--- app/main.py
+++ app/main.py
@@ -1,80 +1,15 @@
-from typing import Annotated
-
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-    },
-}
+import time
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-def fake_hash_pwd(pwd: str):
-    return "fakehashed" + pwd
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-class User(BaseModel):
-    username: str
-    email: str|None = None
-    full_name: str|None = None
-    disabled: bool|None = None
-
-class UserInDB(User):
-    hashed_pwd: str
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-    
-def fake_decode_token(token):
-    user = get_user(fake_users_db, token)
-    return user
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
-    user = fake_decode_token(token=token)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Invalid authentication credentials",
-                            headers={"WWW-Authenticate": "Bearer"})
-    return user
-
-async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]) -> User:
-    if current_user.disabled:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Inactive user")
-    return current_user
-
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends(OAuth2PasswordRequestForm)]):
-    user_dict = fake_users_db.get(form_data.username)
-    print(user_dict)
-    if not user_dict:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Incorrect username")
-    hashed_pwd = fake_hash_pwd(form_data.password)
-    user_dict.update({"hashed_pwd": hashed_pwd})
-    user = UserInDB(**user_dict)
-    if not hashed_pwd == user.hashed_pwd:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Incorrect username or pwd")
-    return {"access_token": user.username, "token_type": "bearer"} #The specs say we must return a json with access_token and token_type
-
-@app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
-    return current_user
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    """
+    Middleware function that is called before each request and after each response
+    """
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
