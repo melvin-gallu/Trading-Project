663aeb18793cb1da6e21d3ca21fc53bd1025d98d
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-24 11:55:51 +0200

    testing

--- app/routeurs/users.py
+++ app/routeurs/users.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+class User(BaseModel):
+    username: str
+
 
 @router.get("/users/", tags=["users"])
 async def read_users():
@@ -15,3 +20,7 @@ async def read_user_me():
 @router.get("/users/{username}", tags=["users"])
 async def read_user(username: str):
     return {"username": username}
+
+@router.post("/users/", tags=["users"])
+async def create_user(user: User) -> list[User]:
+    return user
--- app/test_units.py
+++ app/test_units.py
@@ -14,3 +14,20 @@ def test_read_item_bad_token():
     response = client.get("/", headers={"X-Token": "hailhydra"})
     assert response.status_code == 400
     assert response.json() == {"detail": "Invalid X-Token header"}
+
+
+def test_create_existing_item():
+    response = client.put(
+        "/items/melvin",
+        headers={"X-Token": "fake-secret-token"},
+    )
+    assert response.status_code == 403
+    assert response.json() == {"detail": "You can only update item plumbus"}
+
+def test_create_user():
+    response = client.post(
+        "/users/",
+        headers={"X-Token": "fake-secret-token"},
+        json={"name": "Melvin"}
+    )
+    assert response.status_code == 200
