8357a2866bb00c636e9714892ba7ed2f30eb3cbc
Melvin Gallu <melvin.gallu@seba.swiss>
2024-04-26 09:38:29 +0200

    initialization

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://localhost:8080",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
