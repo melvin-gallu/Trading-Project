5da46cb3224359e6aa1d68265cd97a3421687622
melvin-gallu <melvin.gallu@gmail.com>
2024-08-05 22:27:35 +0200

    react framework

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,jsonify  # Import the Flask class
-app = Flask(__name__)    # Create an instance of the class for our use
-
-@app.route("/", methods=["GET"])
-def say_hello():
-    return jsonify({"msg": "Hello from Flask"})
-
-if __name__ == "__main__":
-    app.run(debug=True)
